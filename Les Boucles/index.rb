e5a7f9dcbf9d37aeaf46f79bdf91a032766dedad
Max Lestage <maxlestage@icloud.com>
2020-09-09 16:12:50 +0200

    add boucle course

---
+++ Les Boucles/index.rb
@@ -0,0 +1,23 @@
+#                                               LES BOUCLES
+
+
+
+
+
+# On commence par la boucle WHILE 
+
+
+# La boucle while s'écrit ainsi: while suivi de sa <condittion>
+# Pour comprendre, on peut traduire l'ensemble par tant que (while) cette <condition>
+
+while <condittion> 
+
+    # NOTRE CODE ..............
+
+
+
+    # tant que le condition sera remplit le code sera executé 
+
+
+    # puis à la fin de notre boucle on met end comme suit :
+end
