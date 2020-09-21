c8754e675df27cd4ced5bce578d23b51dac9042d
Max Lestage <maxlestage@icloud.com>
2020-09-21 17:39:20 +0200

    TP

--- TP_hash/compteur.rb
+++ TP_hash/compteur.rb
@@ -3,7 +3,7 @@
 text = "Un poète est quelqu'un qui dit ou écrit un ou plusieurs poèmes. C'est donc une personne qui possède l'art de combiner les mots, les sonorités, les rythmes pour évoquer des images, suggérer des sensations, des émotions."
 
 # Créer un hash pour stocker les mots
-#   obtenir un tableau des mots de notre texte pour espacer les mots on utilise :a méthode (split) 
+#   obtenir un tableau des mots de notre texte pour espacer les mots on utilise :a méthode (split()) 
 #       Intégrer le tableau précédent 
 #           Remplir le hash 
 #               Afficher les informations
@@ -23,9 +23,11 @@ text = "Un poète est quelqu'un qui dit ou écrit un ou plusieurs poèmes. C'est
 #   On commence par créer un hash pour stocker les mots 
 # Pour créer un hash :
 
-# Notre Var mots égal hash vide 
+# Notre Var frequence égal hashVide 
 
-mots = {}
+frequence = {}
+mots = text.split(' ')
+puts mots.inspect
 
 
 
