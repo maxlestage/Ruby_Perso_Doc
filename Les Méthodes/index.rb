db8632a61041015231bc4db179006b7b417952c7
Max Lestage <maxlestage@icloud.com>
2020-09-24 17:07:58 +0200

    Méthodes exo perso compréhension

--- Les Méthodes/index.rb
+++ Les Méthodes/index.rb
@@ -27,15 +27,33 @@ def convert_to_min(seconds)
         # Et on conclut avec un end comme suit :
 end 
 
-# Si l'on souhaite utilisé notre méthode, il nous suffit maintenant de l'appeler 
-
+# Si l'on souhaite utilisé notre méthode, il nous suffit maintenant de l'appeler, ci dessous avec l'aide du puts :
 puts convert_to_min(778) 
 
+#################################################################################################################################
 
 
 
+#  Ici nous allons créer un petit script qui nous permet de reéuperer le nom de l'utilisateur via le terminal avec la commande 'gets':
+
+#       Alors comme vu plus haut pour débuter une méthode on commence par le mot clé 'def' suivi de notre méthode(notre_parametre) 
+#       Ici nous avons 'bonjour(name)'
 def bonjour(name)
+        # La ligne ci-dessous nous renvoie notre phrase "Bonjour suivi de ce que l'utilisateur entre via la méthode gets que nous allons voir au-dessous"
         return "Bonjour #{name}"
+   # On pense égalment à mettre le end à la fin. 
 end
 
-puts bonjour("Marc")
+
+#       la variable name égal la méthode gets.chomp 
+#                       explication, ces deux methodes, la première gets nous permet de saisir une valeur depuis le terminal et la seconde chomp nous évite le saut de ligne
+#                       à la fin de la saisi du gets ainsi on résume name par l'utilisateur saisi sa valeur puis il valide et la touche enter n'aura pas d'effet sur la saisie. 
+
+name = gets.chomp 
+
+# Ici plus de mystère, affiche donc la méthode bonjour(name)
+# ici litéralement, bonjour la valeur saisie par l'utilisateur. 
+puts bonjour(name)
+
+
+
